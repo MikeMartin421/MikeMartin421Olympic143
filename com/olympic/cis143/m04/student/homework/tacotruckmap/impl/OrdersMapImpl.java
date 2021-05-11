70af459ebd05ec7e71b1f057eba10f65b81d1600
Michael Martin <mic_t93@outlook.com>
2021-05-11 16:00:50 -0700

    Second Push

--- com/olympic/cis143/m04/student/homework/tacotruckmap/impl/OrdersMapImpl.java
+++ com/olympic/cis143/m04/student/homework/tacotruckmap/impl/OrdersMapImpl.java
@@ -4,37 +4,58 @@ import com.olympic.cis143.m04.student.homework.tacotruckmap.OrderDoesNotExistExc
 import com.olympic.cis143.m04.student.homework.tacotruckmap.Orders;
 import com.olympic.cis143.m04.student.homework.tacotruckmap.TacoImpl;
 
+import java.util.ArrayList;
 import java.util.HashMap;
 import java.util.List;
 import java.util.Map;
 
 public class OrdersMapImpl implements Orders {
+	Map<String, List<TacoImpl>> tacoMap = new HashMap<>();
 
     @Override
     public void createOrder(final String orderid) {
+    	List<TacoImpl> taco = new ArrayList<>(); 
+    	tacoMap.put(orderid, taco);
+    	
     }
 
     @Override
     public void addTacoToOrder(final String orderid, final TacoImpl taco) throws OrderDoesNotExistException {
+    	if(tacoMap.containsKey(orderid)) {
+    		List<TacoImpl> tacoList = tacoMap.get(orderid);
+    		tacoList.add(taco);
+    	}
+    	else throw new OrderDoesNotExistException(orderid);
     }
 
     @Override
     public boolean hasNext() {
-        return false;
+    	if(tacoMap.isEmpty())
+    		return false;
+    	return true;
     }
 
     @Override
     public List<TacoImpl> closeOrder(final String orderid) throws OrderDoesNotExistException {
-        return null;
+    	if(tacoMap.containsKey(orderid)) {
+    		List<TacoImpl> tacoList = tacoMap.get(orderid);
+    		tacoMap.remove(orderid);
+    		return tacoList;
+    	}
+    	else throw new OrderDoesNotExistException(orderid);
     }
 
     @Override
     public int howManyOrders() {
-        return 0;
+        return tacoMap.size();
     }
 
     @Override
     public List<TacoImpl> getListOfOrders(final String orderid) throws OrderDoesNotExistException {
-        return null;
+    	if(tacoMap.containsKey(orderid)) {
+    		List<TacoImpl> tacoList = tacoMap.get(orderid);
+    		return tacoList;
+    	}
+    	else throw new OrderDoesNotExistException(orderid);
     }
 }
