83986505ec4afd4bf7911decf9c28b97aefdee3b
Michael Martin <mic_t93@outlook.com>
2021-05-14 15:06:29 -0700

    Second Push M05

--- com/olympic/cis143/m05/student/homework/annotations1/HomeworkAnnotations.java
+++ com/olympic/cis143/m05/student/homework/annotations1/HomeworkAnnotations.java
@@ -1,11 +1,12 @@
 package com.olympic.cis143.m05.student.homework.annotations1;
 
 public class HomeworkAnnotations implements IHomeworkAnnotation {
-
+	@Override
     public String sayHello() {
         return "Hello";
     }
-
+	@SuppressWarnings({"unchecked","Deprecated"})
+	@Deprecated
     public String sayGoodBye() {
         return "Goodbye";
     }
